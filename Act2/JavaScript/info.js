5786eeba229489bf1cc94849649c5a6d5778f3cb
Lingatskie <ellamarielingatong@gmail.com>
2024-03-21 19:18:00 +0800

    mycommit

---
+++ Act2/JavaScript/info.js
@@ -0,0 +1,18 @@
+const upper=(nav1,nav2,nav3,Title, Paragraph,Cbtn)=>{
+  let div= document.createElement("div")
+  div.className="up"
+  div.innerHTML=`
+      <nav class="navi">
+      <ul>
+        <li><a href="/Home/index.html">${nav1}<a></li>
+        <li><a href="/What/index.html">${nav2}</a></li>
+        <li><a href="/Activity1/index.html">${nav3}</a></li>
+      </ul>
+      </nav>
+                  <h1>${Title}</h1>
+                  <p>${Paragraph}</p>
+                  <button>${Cbtn}</button>
+  `
+  return div
+}
+export{upper}
